88ae46df8423a955b72e3efbf3c597b1630a9c9c
Marlon Werth <werthmar@gmail.com>
2023-06-25 12:22:43 +0200

    tooltips added

--- components/Navbar.js
+++ components/Navbar.js
@@ -5,6 +5,7 @@ import Link from "next/link";
 import Image from "next/image";
 import { RiArrowGoBackLine } from 'react-icons/ri';
 import { BsArrowBarLeft, BsArrowBarRight } from 'react-icons/bs';
+import { Tooltip } from '@mui/material';
 
 export default class CustomNavbar extends React.Component
 {
@@ -22,7 +23,8 @@ export default class CustomNavbar extends React.Component
                 props.pageOrder[3],
             ],
             pageIndex: props.pageIndex,
-            title: props.title
+            title: props.title,
+            nextPageButtonActive: false
         };
     }
 
@@ -33,6 +35,10 @@ export default class CustomNavbar extends React.Component
 
     setPageIndex( index ) {
         this.setState({ pageIndex: index });
+    }
+
+    setNextPageButtonActive( bool ) {
+        this.setState({ nextPageButtonActive: bool });
     }
 
     // Dynamicly create the page navigation buttons based on the current index/page the user is on
@@ -75,11 +81,13 @@ export default class CustomNavbar extends React.Component
 
                                     {/* Home button */}
                                     <Col>
-                                        <Link href="/">
-                                            <Button className='homeButton'>
-                                                <Image src="/icons/Menü_Haus.png" width={120} height={65} alt='HomeButton' />
-                                            </Button>
-                                        </Link>
+                                        <Tooltip arrow title="home">
+                                            <Link href="/">
+                                                <Button className='homeButton'>
+                                                    <Image src="/icons/Menü_Haus.png" width={120} height={65} alt='HomeButton' />
+                                                </Button>
+                                            </Link>
+                                        </Tooltip>
                                     </Col>
 
                                     {/* Page Buttons */}
@@ -88,9 +96,13 @@ export default class CustomNavbar extends React.Component
                                     </Col>
 
                                     {/* Back Buttons */}
-                                    <Button onClick={ () => this.props.back() } className="backButton">
-                                        <RiArrowGoBackLine size={45} color="grey" />
-                                    </Button>
+                                    <Tooltip arrow title="back">
+                                        <div>
+                                            <Button onClick={ () => this.props.back() } className="backButton">
+                                                <RiArrowGoBackLine size={45} color="grey" />
+                                            </Button>
+                                        </div>
+                                    </Tooltip>
 
 
                         </Col>
@@ -120,8 +132,16 @@ export default class CustomNavbar extends React.Component
                                     </Col>
 
                                     <Col xs="12">
-                                        {/* The callback is the nextPage function from the advisor page which is passed into this class  */}
-                                        <Button onClick={ () => this.props.nextPage() }>GO ON</Button>
+                                        <Tooltip arrow title="Please select at least 1 item from the list in order to proceed.">
+                                            <div>
+                                                {/* The callback is the nextPage function from the advisor page which is passed into this class  */}
+                                                <Button
+                                                    className={ this.state.nextPageButtonActive ? "" : "disabled" }
+                                                    onClick={ () => this.props.nextPage() }>
+                                                    GO ON
+                                                </Button>
+                                            </div>
+                                        </Tooltip>
                                     </Col>
 
 
