386846c65f6d4cd908c41d38a4d5aef3dc3ae5cf
Marlon Werth <werthmar@gmail.com>
2023-07-26 13:02:48 +0200

    main page

--- pages/index.js
+++ pages/index.js
@@ -8,6 +8,8 @@ import {
   Button,
 } from "reactstrap";
 import { useRouter } from 'next/router'
+import Image from "next/image";
+import { Tooltip } from '@mui/material';
 
 export default function IndexPage()
 {
@@ -21,33 +23,69 @@ export default function IndexPage()
         <Col xs="3" className="buttonCol">
           <Row xs={1} style={{margin: 0}}> 
             <Col style={{paddingBottom: "10px"}}>
-              <Button className="navigationButton roadmap">
-                Roadmap
-              </Button>
+              <Tooltip arrow title="Roadmap" style={{width: "100%", height: "100%" }}>
+              <div>
+                <Button className="navigationButton">
+                  <div className="backgroundImage roadmap">
+                    <Image src={"/icons/Roadmap.png" }
+                      width={130}
+                      height={130}
+                      alt="Roadmap"
+                      />
+                  </div>
+                </Button>
+              </div>
+              </Tooltip>
             </Col>
     
             <Col style={{paddingTop: "10px"}}>
-              <Button className="navigationButton">
-                <div className="whiteBlock">
-                  test
+              <Tooltip arrow title="Library" style={{width: "100%", height: "100%" }}>
+                <div>
+                  <Button className="navigationButton">
+                    <div className="backgroundImage library">
+                      <Image src={"/icons/Bibliothek.png" }
+                        width={130}
+                        height={130}
+                        alt="Library"
+                        />
+                    </div>
+                  </Button>
                 </div>
-              </Button>
+              </Tooltip>
             </Col>
           </Row>
         </Col>
 
         <Col xs="6" className="buttonCol">
-          <Button className="navigationButton" onClick={ () => router.push("/process") } >
-            <div className="whiteBlock">
-              New Project
+          <Tooltip arrow title="New Project" style={{width: "100%", height: "100%" }}>
+            <div>
+              <Button className="navigationButton" onClick={ () => router.push("/process") } >
+                <div className="backgroundImage newProject">
+                  <Image src={"/icons/NeuesProjekt.png" }
+                    width={130}
+                    height={130}
+                    alt="New Project"
+                    />
+                </div>
+              </Button>
             </div>
-          </Button>
+          </Tooltip>  
         </Col>
 
         <Col xs="3" className="buttonCol">
-          <Button className="navigationButton">
-          test
-          </Button>
+          <Tooltip arrow title="Project Archive" style={{width: "100%", height: "100%" }}>
+            <div>
+              <Button className="navigationButton">
+                <div className="backgroundImage archive">
+                  <Image src={"/icons/Projektarchiv.png" }
+                    width={130}
+                    height={130}
+                    alt="Project Archive"
+                    />
+                </div>
+              </Button>
+            </div>
+          </Tooltip>  
         </Col>
 
       </Row>
